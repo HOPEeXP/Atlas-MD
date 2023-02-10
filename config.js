004b6229f47236be5dccdf42bd22aa9bf1945417
HOPE <101519089+HOPEeXP@users.noreply.github.com>
2023-02-10 14:09:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ global.port = process.env.PORT || "8000";
 // ---------------------Do Not Modify this part------------------- //
 
 global.mess = {
-  jobdone: "Job done...",
+  jobdone: "Done 🌟",
   useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
   botadmin:
     "Sorry, i cant execute this command without being an *Admin* of this group.",
@@ -64,7 +64,7 @@ global.mess = {
   grouponly: "This command is only made for *Groups*, Baka!",
   privateonly: "This command is only made for *Private Chat*, Baka!",
   botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
+  waiting: "Sending ⚡",
   nolink: "Please provide me *link*, Baka!",
   error: "An error occurd!",
   banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
